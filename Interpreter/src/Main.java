aa1c23f71601cb6d09cbaca6919840a9f0fcff7c
Rf <repedraza@unicesar.edu.co>
2024-06-22 16:22:22 +0200

    v2 otras expresiones

--- Interpreter/src/Main.java
+++ Interpreter/src/Main.java
@@ -2,13 +2,19 @@
 public class Main {
     public static void main(String[] args) {
         // Crear las expresiones
-        Expression expression = new SubtractExpression(
-                new AddExpression(new NumberExpression(8), new NumberExpression(2)),
-                new NumberExpression(1)
-        );
+        Expression expression = buildExpression();
 
         // Interpretar y obtener el resultado
         int result = expression.interpret();
-        System.out.println("Resultado: " + result); // Resultado: 4
+        System.out.println("Resultado: " + result); // Resultado: 10
+    }
+
+    public static Expression buildExpression() {
+        // Aquí puedes construir cualquier expresión compleja que desees evaluar
+        Expression exp1 = new AddExpression(new NumberExpression(2), new NumberExpression(3));
+        Expression exp2 = new SubtractExpression(exp1, new NumberExpression(1));
+        Expression exp3 = new AddExpression(exp2, new NumberExpression(6));
+
+        return exp3;
     }
 }
